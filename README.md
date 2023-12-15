831eebea32bb486b4c8820021f2b3a68074b34fc
Avaneesh Tiwari <avaneesh.tiwari@cradlepoint.com>
2023-12-15 12:19:05 +0530

    Readme added.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # employee-demo
-Find the Average Salary for a City and Designation
+Find the Average Salary for Location and Designation.
+1. I've used concurrent Map and Parallel Stream to work on Parallel.
+2. Used groupingByConcurrent() method to group by Location first and then by Salary.
+3. Used foreach() method to print the Values.
--- src/main/java/org/example/FindAverageSalaryApp.java
+++ src/main/java/org/example/FindAverageSalaryApp.java
@@ -8,12 +8,12 @@ class FindAverageSalaryApp{
 
     public void findAverageSalary(List<Employee> employees){
 
-        ConcurrentMap<String, ConcurrentMap<String, Double>> result = employees.parallelStream()
+        ConcurrentMap<String, ConcurrentMap<String, Double>> locationToDesignationMap = employees.parallelStream()
                 .collect(Collectors.groupingByConcurrent(Employee::getOfficeLocation,
                         Collectors.groupingByConcurrent(Employee::getDesignation,
                                 Collectors.averagingDouble(Employee::getSalary))));
 
-        result.forEach((location, designationMap) -> {
+        locationToDesignationMap.forEach((location, designationMap) -> {
             designationMap.forEach((designation, avgSalary) -> {
                 System.out.printf("%s --> %s --> %.2f\n", location, designation, avgSalary);
             });
